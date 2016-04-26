baeb1ebc732e8a24830fe54aafe78d0066da0115
Fahad Al-Mutairi <fahadalmutairi@outlook.com>
2016-04-26 19:31:44 +0300

    Add basic HTML templates and property list and detail view

--- main/views.py
+++ main/views.py
@@ -8,4 +8,10 @@ def property_list(request):
 	context['properties'] = properties
 	return render(request, 'properties_list.html', context)
 
+def property_detail(request, pk):
+	context = {}
+	property_object = Property.object.get(pk=pk)
+	context['property']	= property_object
+	return render(request,'property_detail.html', context)
+
 	 
