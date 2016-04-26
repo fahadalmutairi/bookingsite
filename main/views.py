5a6487851e102f51b44b28bf39e2e8db602a6a76
Fahad Al-Mutairi <fahadalmutairi@outlook.com>
2016-04-26 19:22:12 +0300

    no message

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def property_list(request):
+	context = {}
+	properties = Property.objects.all()
+	context['properties'] = properties
+	return render(request, 'properties_list.html', context)
+
+	 
